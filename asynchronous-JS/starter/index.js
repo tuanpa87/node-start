86d54ad85858848f8f319c537a9de339ef4bd465
tuanpa87 <tuanpa87@users.noreply.github.com>
2022-08-23 01:14:22 +0700

    Waiting for Multiple Promises Simultaneously

--- asynchronous-JS/starter/index.js
+++ asynchronous-JS/starter/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-const readFilePro = (file) => {
+const readFilePro = file => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, (err, data) => {
       if (err) reject('I could not find that file 😢');
@@ -12,32 +12,41 @@ const readFilePro = (file) => {
 
 const writeFilePro = (file, data) => {
   return new Promise((resolve, reject) => {
-    fs.writeFile(file, data, (err) => {
+    fs.writeFile(file, data, err => {
       if (err) reject('Could not write file 😢');
       resolve('success');
     });
   });
 };
 
-getDogPic = async () => {
+const getDogPic = async () => {
   try {
     const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    const res = await superagent.get(
+    const res1Pro = superagent.get(
       `https://dog.ceo/api/breed/${data}/images/random`
     );
-    console.log(res.body.message);
-    await writeFilePro('dog-img.txt', res.body.message);
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const imgs = all.map(el => el.body.message);
+    console.log(imgs);
+
+    await writeFilePro('dog-img.txt', imgs.join('\n'));
     console.log('Random dog image saved to file!');
   } catch (err) {
     console.log(err);
 
     throw err;
   }
-
   return '2: READY 🐶';
 };
+
 (async () => {
   try {
     console.log('1: Will get dog pics!');
@@ -60,7 +69,6 @@ getDogPic()
     console.log('ERROR 💥');
   });
 */
-
 /*
 readFilePro(`${__dirname}/dog.txt`)
   .then(data => {
