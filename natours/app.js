d99462aa1d89a355a41420aee70b6b5bdff9e7e9
tuanpa87 <tuanpa87@users.noreply.github.com>
2022-09-01 22:12:24 +0700

    Setting up ESLint + Prettier in VS Code

--- natours/app.js
+++ natours/app.js
@@ -1,30 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware wave 👋');
   next();
-})
+});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
-})
-
+});
 
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
