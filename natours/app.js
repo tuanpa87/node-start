9ee6375d37f0198dc53da0e267293d1ca073a0ad
tuanpa87 <tuanpa87@users.noreply.github.com>
2022-09-01 17:42:11 +0700

    Using 3rd-Party Middleware

--- natours/app.js
+++ natours/app.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1) MIDDLEWAREs
+app.use(morgan('dev'))
 
 app.use(express.json());
 
@@ -18,6 +22,8 @@ app.use((req, res, next) => {
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// 2) ROUTE HANDLERS
 
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -105,6 +111,8 @@ const deleteTour = (req, res) => {
   });
 };
 
+// 3) ROUTES
+
 // app.get('/api/v1/tours', getAllTours);
 // app.get('/api/v1/tours/:id', getTour);
 // app.post('/api/v1/tours', createTour);
@@ -121,6 +129,9 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+
+// 4) START SERVER
 
 const port = 3000;
 app.listen(port, () => {
