5684b7834195cf36677bdedc9fbec37622fc4b45
tuanpa87 <tuanpa87@users.noreply.github.com>
2022-09-02 23:58:47 +0700

    Making the API Better: Pagination

--- natours/controllers/tourControllers.js
+++ natours/controllers/tourControllers.js
@@ -34,6 +34,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+    // page=2&limit=10
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    if(req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
+    query = query.skip(skip).limit(limit);
+
     // EXECUTE QUERY
     const tours = await query;
 
