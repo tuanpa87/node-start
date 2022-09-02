a4508e5ce1c029715958c40a4f4cf99abc728a41
tuanpa87 <tuanpa87@users.noreply.github.com>
2022-09-02 16:16:31 +0700

    Updating Documents

--- natours/controllers/tourControllers.js
+++ natours/controllers/tourControllers.js
@@ -61,13 +61,25 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<updated tour here ...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
