807a49b3cb8cccc46a1425dabb06023862d800c8
tuanpa87 <tuanpa87@users.noreply.github.com>
2022-09-04 20:50:35 +0700

    Data Validation: Built-In Validators

--- natours/controllers/tourControllers.js
+++ natours/controllers/tourControllers.js
@@ -92,7 +92,7 @@ exports.updateTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!',
+      message: err,
     });
   }
 };
