608ef0cd77bffcd34fa6182e008a117b2597efc2
tuanpa87 <tuanpa87@users.noreply.github.com>
2022-09-24 16:47:34 +0700

    Implementing a Global Error Handling Middleware

--- natours/app.js
+++ natours/app.js
@@ -25,9 +25,23 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl}`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl}`,
+  // });
+  const err = new Error(`Can't fint ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
