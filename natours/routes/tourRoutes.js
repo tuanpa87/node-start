70c80844d865108f7b8496a88918ec8c3c135b45
tuanpa87 <tuanpa87@users.noreply.github.com>
2022-10-16 10:01:04 +0700

    Implementing Simple Nested Routes

--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewController = require('./../controllers/reviewController');
 
 const router = express.Router();
 
@@ -31,6 +32,16 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
+  );
+
+// POST /tour/234fad4/reviews
+// GET /tour/234fad4/reviews
+router
+  .route('/:tourId/reviews')
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
   );
 
 module.exports = router;
