4b8056736ee5235f3ffc698da6d5fffe2868783f
tuanpa87 <tuanpa87@users.noreply.github.com>
2022-10-16 14:25:17 +0700

    Adding a /me Endpoint

--- natours/controllers/userController.js
+++ natours/controllers/userController.js
@@ -11,6 +11,23 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+});
+
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
